de34a750af6e1d24e45d4257dba15404919f00d4
rhorn86 <rhorn86@gmail.com>
2026-09-18 20:06:18 -0500

    Complete CH1 L4

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
+def main():
+
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if not api_key:
+        raise RuntimeError("No API key was provided")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use on paragraph maximum.",
+            }
+        ],
+    )
+
+    if not response.usage:
+        raise RuntimeError("No usage object was returned in the response")
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+    print(f"Response: {response.choices[0].message.content}")
 
 if __name__ == "__main__":
     main()
