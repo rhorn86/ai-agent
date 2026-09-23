aff95e4988b66c697e74caca7ee27fc1e96c2221
rhorn86 <rhorn86@gmail.com>
2026-09-22 22:22:07 -0500

    Completed 4.1: Agent Loop

--- call_function.py
+++ call_function.py
@@ -1,9 +1,11 @@
+import json
+from collections.abc import Callable
+
+from config import WORKING_DIR
 from functions.get_files_info import schema_get_files_info, get_files_info
 from functions.get_file_content import schema_get_file_content, get_file_content
 from functions.run_python_file import schema_run_python_file, run_python_file
 from functions.write_file import schema_write_file, write_file
-import json
-from collections.abc import Callable
 
 available_functions = [
     schema_get_files_info,
@@ -11,6 +13,13 @@ available_functions = [
     schema_run_python_file,
     schema_write_file,
 ]
+
+function_map: dict[str, Callable[..., str]] = {
+    "get_file_content": get_file_content,
+    "get_files_info": get_files_info,
+    "run_python_file": run_python_file,
+    "write_file": write_file,
+}
 
 def call_function(tool_call, verbose: bool = False) -> dict:
     function_name = tool_call.function.name
@@ -20,13 +29,6 @@ def call_function(tool_call, verbose: bool = False) -> dict:
     else:
         print(f" - Calling function: {function_name}")
 
-    function_map: dict[str, Callable[..., str]] = {
-        "get_file_content": get_file_content,
-        "get_files_info": get_files_info,
-        "run_python_file": run_python_file,
-        "write_file": write_file,
-    }
-
     if function_name not in function_map:
         return {
             "role": "tool",
@@ -34,7 +36,7 @@ def call_function(tool_call, verbose: bool = False) -> dict:
             "content": f"Error: Unknown function: {function_name}",
         }
 
-    function_args["working_directory"] = "./calculator"
+    function_args["working_directory"] = WORKING_DIR
     result = function_map[function_name](**function_args)
 
     return {
@@ -42,4 +44,3 @@ def call_function(tool_call, verbose: bool = False) -> dict:
         "tool_call_id": tool_call.id,
         "content": result,
     }
-
--- main.py
+++ main.py
@@ -4,46 +4,52 @@ from openai import OpenAI
 import argparse
 from prompts import system_prompt
 from call_function import available_functions, call_function
+from openai.types.chat import ChatCompletion
 
-def generate_content(client, messages, args):
+def call_model(client: OpenAI, messages: list) -> ChatCompletion:
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=messages,
         tools=available_functions,
     )
     if not response.usage:
-        raise RuntimeError("No usage object was returned in the response")
+        raise RuntimeError("API response appears to be malformed")
+
+    return response
+
+def generate_content(response: ChatCompletion, messages: list, verbose: bool) -> bool:
+
+    if verbose:
+        print("Prompt tokens:", response.usage.prompt_tokens if response.usage else "Unkown")
+        print("Response tokens:", response.usage.completion_tokens if response.usage else "Unkown")
 
     message = response.choices[0].message
-
-    if args.verbose:
-        print("User prompt:")
-        print(args.user_prompt)
-        print(f"\nPrompt tokens: {response.usage.prompt_tokens}")
-        print(f"Response tokens: {response.usage.completion_tokens}\n")
-    if message.tool_calls:
-        for tool_call in message.tool_calls:
-            result_message = call_function(tool_call, args.verbose)
-            if result_message["content"] == "":
-                raise Exception("Error: tool message has an empty 'content' field")
-            if args.verbose:
-                print(f"-> {result_message['content']}")
-
-    else:
+    if not message.tool_calls:
         print("Response:")
-        print(response.choices[0].message.content)
+        print(message.content)
+        return False
+
+    for tool_call in message.tool_calls:
+        if tool_call.type != "function":
+            continue
+        result_message = call_function(tool_call, verbose)
+        if not result_message.get("content"):
+            raise RuntimeError(f"Empty function response for {tool_call.function.name}")
+        messages.append(result_message)
+        if verbose:
+            print(f"-> {result_message['content']}")
+    return True
 
 def main() -> None:
-
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to the LLM")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     load_dotenv()
     api_key = os.environ.get("OPENROUTER_API_KEY")
     if not api_key:
-        raise RuntimeError("No API key was provided")
+        raise RuntimeError("OPENROUTER_API_KEY environment variable not set")
 
     client = OpenAI(
         base_url="https://openrouter.ai/api/v1",
@@ -53,7 +59,18 @@ def main() -> None:
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": args.user_prompt},
     ]
-    generate_content(client, messages, args)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+
+    for _ in range(20):
+        response = call_model(client, messages)
+        message = response.choices[0].message
+        messages.append(message)
+        if not generate_content(response, messages, args.verbose):
+            break
+    else:
+        print("Error: Maximium number of iterations occurred with no response")
+        exit(1)
 
 
 if __name__ == "__main__":
